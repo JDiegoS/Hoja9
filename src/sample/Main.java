404b07eca460f17c066a49fc6103053cae7c380a
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-04-27 00:00:29 -0600

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,21 +13,40 @@ public class Main {
         SplayTree sTree = new SplayTree();
         RedBlackTree rTree = new RedBlackTree();
 
-        System.out.println("\nQue implementacion desea usar?\n");
-        System.out.println("1. SplayTree ");
-        System.out.println("2. RBT ");
-        System.out.println("3. Salir");
+        try{
+            File file = new File("freedict-eng-spa.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String text;
+            while((text = br.readLine()) != null){
+                text = text.replace("(", "");
+                text = text.replace(")", "\n");
+                String[] parte = text.split(", ");
+                String clave = parte[0];
+                String valor = parte[1];
 
-        int ingreso = sc.nextInt();
-        if (ingreso == 1){
-            sTree.printSorted();
-        }
-        else if(ingreso == 2){
-            rTree.printTree(root);
+
+                System.out.println("\nQue implementacion desea usar?\n");
+                System.out.println("1. SplayTree ");
+                System.out.println("2. RBT ");
+                System.out.println("3. Salir");
+
+                int ingreso = sc.nextInt();
+                if (ingreso == 1){
+                    sTree.printSorted();
+                }
+                else if(ingreso == 2){
+                    rTree.printTree(root);
 
 
-        }else if(ingreso == 3){
-            System.out.println("Gracias por utilizar el programa");
+                }else if(ingreso == 3){
+                    System.out.println("Gracias por utilizar el programa");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
